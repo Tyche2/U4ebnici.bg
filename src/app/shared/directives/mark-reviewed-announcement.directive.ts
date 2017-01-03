4484fa035c0a4162e102159b958e9e6cb0ef2e21
doubleshot1 <nenost12@gmail.com>
2017-01-03 20:16:55 +0200

    Make MarkReviewedAnnouncementDirective to set glyphicon-eye-open to reviewed announcements

--- src/app/shared/directives/mark-reviewed-announcement.directive.ts
+++ src/app/shared/directives/mark-reviewed-announcement.directive.ts
@@ -21,9 +21,11 @@ export class MarkReviewedAnnouncementDirective implements AfterViewInit {
   ngAfterViewInit() {
     this.reviewedAnnouncements = this.localStorageService.getCollection('reviewed-announcements');
     this.elementId = this.element.nativeElement.getAttribute('data-id');
-    
+
     if (this.reviewedAnnouncements.indexOf(this.elementId) > -1) {
-      this.element.nativeElement.classList.add(this.reviewedClassSelector);
+      let reviewedElementSpan = this.element.nativeElement.getElementsByClassName('reviewed-announcement-span')[0];
+      console.log(reviewedElementSpan);
+      reviewedElementSpan.classList.remove('hidden');
     }
   }
 }
