9742dfc018800dd41e3302d38764feeb18c62f52
Strateva <MiroslavaStrateva@gmail.com>
2017-01-03 10:28:03 +0200

    Fix starting of app

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(success => console.log('Bootstrap success'))
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
+//  .then(success => console.log('Bootstrap success'))
+//  .catch(err => console.log(err));
